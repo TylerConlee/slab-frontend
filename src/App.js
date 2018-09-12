22f59464f13836492fffc3e63a2cc24cb6cfed8f
Tyler Conlee <tylerlconlee@gmail.com>
2018-09-12 13:33:46 -0700

    add initial health call

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({isLoading: true});
+    fetch('localhost:8090/')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
   render() {
+    const { data } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +31,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <ul>
+            <li>Uptime: {data.uptime}</li>
+            <li>Version: {data.version}</li>
+          </ul>
         </p>
       </div>
     );
