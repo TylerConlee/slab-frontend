fd44fc097fbb77a10b9f04706e2bf898db160139
Tyler Conlee <tylerlconlee@gmail.com>
2018-09-13 07:08:18 -0700

    change default text on home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import Health from './components/health/health'
 
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Slab</h1>
         </header>
         <p className="App-intro">
           <Health />
