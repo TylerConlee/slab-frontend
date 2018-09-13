380cab05b4fd96f9f56b35451cbfd1c60fee1d01
Tyler Conlee <tylerlconlee@gmail.com>
2018-09-13 06:29:58 -0700

    move health to separate component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Health from './components/health/health.js'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: null,
-      isLoading: false,
-    };
-  }
-
-  componentDidMount() {
-    this.setState({isLoading: true});
-    fetch('/api')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ data, isLoading: false });
-        console.log(data);
-      });
-    
-  }
+  
   render() {
-    const { data,isLoading } = this.state;
-
-    if (isLoading) {
-      console.log(data)
-      return <p>Loading...</p>;
-    }
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -36,10 +14,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          <ul>
-            <li>Uptime: {data.uptime}</li>
-            <li>Version: {data.version}</li>
-          </ul>
+          <Health></Health>
         </p>
       </div>
     );
